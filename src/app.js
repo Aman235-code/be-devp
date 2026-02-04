f426f033cda267910fb330eec5de6b1a00c98772
Aman Ahamed <amanahamed04@gmail.com>
2026-02-04 17:05:15 +0530

    creation of server and notes CRUD

---
+++ src/app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const notes = [];
+
+app.post("/notes", (req, res) => {
+  notes.push(req.body);
+  res.status(201).json({
+    message: "Note created successfully",
+  });
+});
+
+app.get("/notes", (req, res) => {
+  res.status(200).json({
+    notes: notes,
+  });
+});
+
+app.delete("/notes/:index", (req, res) => {
+  const index = req.params.index;
+  delete notes[index];
+  res.status(200).json({
+    message: "Note deleted successfully",
+  });
+});
+
+app.patch("/notes/:index", (req, res) => {
+  const index = req.params.index;
+  const { description } = req.body;
+  notes[index].description = description;
+
+  res.status(200).json({
+    message: "Note updated successfully",
+  });
+});
+
+module.exports = app;
+
+// htvNowmVKeJnj6Rp
+// mongodb+srv://yt:htvNowmVKeJnj6Rp@cluster0.skopojj.mongodb.net/
