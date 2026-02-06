144a0f92896ef28e23524115fb3a7c4cfbc6d552
Aman Ahamed <amanahamed04@gmail.com>
2026-02-06 15:32:24 +0530

    create post api

--- proj1/backend/src/app.js
+++ proj1/backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const multer = require("multer");
 const uploadFile = require("../services/storage.service.js");
+const postModel = require("./models/post.model.js");
 
 const app = express();
 app.use(express.json());
@@ -8,10 +9,17 @@ app.use(express.json());
 const upload = multer({ storage: multer.memoryStorage() });
 
 app.post("/create-post", upload.single("image"), async (req, res) => {
-  console.log(req.body);
-  console.log(req.file);
   const result = await uploadFile(req.file.buffer);
-  console.log(result);
+
+  const post = await postModel.create({
+    image: result.url,
+    caption: req.body.caption,
+  });
+
+  return res.status(201).json({
+    message: "Post Created Successfully",
+    post
+  })
 });
 
 module.exports = app;
