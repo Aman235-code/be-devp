7021dea8bfcc8803209d9a131ae9f14b64f0f316
Aman Ahamed <amanahamed04@gmail.com>
2026-02-07 11:51:05 +0530

    proj 2 setup

---
+++ proj2/server/src/db/db.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+async function connectDb() {
+  await mongoose.connect(`${process.env.MONGO_URI}/project2`);
+  console.log("Connected to db");
+}
+
+module.exports = connectDb;
