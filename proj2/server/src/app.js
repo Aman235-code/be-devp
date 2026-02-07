9272132e05c12a5ec554257e632423ca8772c226
Aman Ahamed <amanahamed04@gmail.com>
2026-02-07 12:20:44 +0530

    cookie parser

--- proj2/server/src/app.js
+++ proj2/server/src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const authroutes = require("./routes/auth.routes.js");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authroutes);
 
 module.exports = app;
