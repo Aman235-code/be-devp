f8faf063fa17982424a12d3aa144c2a6c6ca512d
Aman Ahamed <amanahamed04@gmail.com>
2026-02-07 12:05:19 +0530

    update route

--- proj2/server/src/app.js
+++ proj2/server/src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const authroutes = require("./routes/auth.routes.js");
 
 const app = express();
 app.use(express.json());
+app.use("/api/auth", authroutes);
 
 module.exports = app;
