cbbbd6be2893e3bac5d272b8d6b6838064dbd910
Aman Ahamed <amanahamed04@gmail.com>
2026-02-07 11:52:55 +0530

    updated db function

--- proj2/server/src/db/db.js
+++ proj2/server/src/db/db.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 
 async function connectDb() {
-  await mongoose.connect(`${process.env.MONGO_URI}/project2`);
-  console.log("Connected to db");
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}/project2`);
+    console.log("Connected to db");
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = connectDb;
